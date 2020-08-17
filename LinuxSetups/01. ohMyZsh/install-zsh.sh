28ff09ce9aa5257293161f7d2beb72f9e075deb1
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-17 17:46:46 +0600

    Updates

--- LinuxSetups/01. ohMyZsh/install-zsh.sh
+++ LinuxSetups/01. ohMyZsh/install-zsh.sh
@@ -6,7 +6,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 apt-get install build-essential libssl-dev
 sudo apt install zsh
 sudo chsh -s $(which zsh)
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 chsh -s $(which zsh)
