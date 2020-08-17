c16dd4ba67de5015da3e2cb36ef0273435681ecc
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-17 22:41:52 +0600

    - changes on the install-zsh.sh

--- linux-setups/01. ohMyZsh/install-zsh.sh
+++ linux-setups/01. ohMyZsh/install-zsh.sh
@@ -5,7 +5,11 @@ sudo apt update
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 apt-get install build-essential libssl-dev
 sudo apt install zsh
-sudo chsh -s $(which zsh)
+yes | cp -rf .zshrc ~/.zshrc
+sudo apt update
+source ~/.zshrc
+cat ~/.zshrc
+chsh -s `which zsh`
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
@@ -15,8 +19,7 @@ yes | cp -rf .zshrc ~/.zshrc
 sudo apt update
 source ~/.zshrc
 cat ~/.zshrc
-chsh -s $(which zsh)
-sudo chsh -s $(which zsh)
+chsh -s `which zsh`
 read -s -n 1 -p "Press any key to reboot the OS . . ."
 sudo reboot
 
